cc0efa2982bb808a40bc2792c667c05d08ea02e5
YU-KAI HUANG <edwall201@gmail.com>
2024-07-22 22:28:26 +0800

    update llm

--- rag.py
+++ rag.py
@@ -1,3 +1,32 @@
 from langchain.document_loaders import PyMuPDFLoader
-loader = PyMuPDFLoader("")
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import Chroma
+from langchain.callbacks.manager import CallbackManager
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+from langchain_community.llms import LlamaCpp
+
+import sqlite3
+print(sqlite3.sqlite_version)
+
+
+loader = PyMuPDFLoader("./Alex.pdf")
 PDF_data = loader.load()
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=100, chunk_overlap=5)
+all_splits = text_splitter.split_documents(PDF_data)
+model_name = "sentence-transformers/all-MiniLM-L6-v2"
+model_kwargs = {'device': 'cpu'}
+embedding = HuggingFaceEmbeddings(model_name=model_name, model_kwargs=model_kwargs)
+persist_directory = 'db'
+vectordb = Chroma.from_documents(documents=all_splits, embedding=embedding, persist_directory=persist_directory)
+model_path = "llama.cpp/models/llama-2-7b-chat/llama-2_q4.gguf"
+llm = LlamaCpp(
+    model_path=model_path,
+    n_gpu_layers=100,
+    n_batch=512,
+    n_ctx=2048,
+    f16_kv=True,
+    callback_manager=CallbackManager([StreamingStdOutCallbackHandler()]),
+    verbose=True,
+)
